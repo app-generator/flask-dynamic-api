dad34e9598d138e0f19b784ead29e82a81087150
Mahmood Choopani <choopani.m81@gmail.com>
2022-07-20 12:19:35 +0430

    add rest login view (TODO: convert to flask-restx implementation)

--- apps/authentication/routes.py
+++ apps/authentication/routes.py
@@ -2,8 +2,8 @@
 """
 Copyright (c) 2019 - present AppSeed.us
 """
-
-from flask import render_template, redirect, request, url_for
+import jwt
+from flask import render_template, redirect, request, url_for, current_app
 from flask_login import (
     current_user,
     login_user,
@@ -16,40 +16,6 @@ from apps.authentication.forms import LoginForm, CreateAccountForm
 from apps.authentication.models import Users
 
 from apps.authentication.util import verify_pass
-
-@blueprint.route('/')
-def route_default():
-    return redirect(url_for('authentication_blueprint.login'))
-
-# Login & Registration
-
-@blueprint.route('/login', methods=['GET', 'POST'])
-def login():
-    login_form = LoginForm(request.form)
-    if 'login' in request.form:
-
-        # read form data
-        username = request.form['username']
-        password = request.form['password']
-
-        # Locate user
-        user = Users.query.filter_by(username=username).first()
-
-        # Check the password
-        if user and verify_pass(password, user.password):
-
-            login_user(user)
-            return redirect(url_for('authentication_blueprint.route_default'))
-
-        # Something (user or pass) is not ok
-        return render_template('accounts/login.html',
-                               msg='Wrong user or password',
-                               form=login_form)
-
-    if not current_user.is_authenticated:
-        return render_template('accounts/login.html',
-                               form=login_form)
-    return redirect(url_for('home_blueprint.index'))
 
 
 @blueprint.route('/register', methods=['GET', 'POST'])
@@ -96,25 +62,43 @@ def register():
 @blueprint.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('authentication_blueprint.login')) 
+    return redirect(url_for('authentication_blueprint.login'))
 
-# Errors
-
-@login_manager.unauthorized_handler
-def unauthorized_handler():
-    return render_template('home/page-403.html'), 403
-
-
-@blueprint.errorhandler(403)
-def access_forbidden(error):
-    return render_template('home/page-403.html'), 403
-
-
-@blueprint.errorhandler(404)
-def not_found_error(error):
-    return render_template('home/page-404.html'), 404
-
-
-@blueprint.errorhandler(500)
-def internal_error(error):
-    return render_template('home/page-500.html'), 500
+@blueprint.route("/login", methods=["POST"])
+def login():
+    try:
+        data = request.json
+        if not data:
+            return {
+                       'message': 'username or password is missing',
+                       "data": None,
+                       'success': False
+                   }, 400
+        # validate input
+        user = Users.query.filter_by(username=data.get('username')).first()
+        if user and verify_pass(data.get('password'), user.password):
+            try:
+                # token should expire after 24 hrs
+                user.api_token = jwt.encode(
+                    {"user_id": user.id},
+                    current_app.config["SECRET_KEY"],
+                    algorithm="HS256"
+                )
+                return {
+                    "message": "Successfully fetched auth token",
+                    "data": user.api_token
+                }
+            except Exception as e:
+                return {
+                           "error": "Something went wrong",
+                           "message": str(e)
+                       }, 500
+        return {
+                   'message': 'username or password is wrong',
+                   'success': False
+               }, 403
+    except Exception as e:
+        return {
+                   "error": "Something went wrong",
+                   "message": str(e)
+               }, 500
