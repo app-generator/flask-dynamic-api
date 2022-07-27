b1cb8c31d62065e96c7fb7d1e7ce97f1dc38b592
App Generator <51070104+app-generator@users.noreply.github.com>
2022-07-27 17:33:47 +0300

    Minor update on Book model

--- apps/models.py
+++ apps/models.py
@@ -3,7 +3,7 @@ from apps import db
 
 class Book(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
+    name = db.Column(db.String(80), nullable=False)
 
     def __repr__(self):
         return '<Book %r>' % self.name
